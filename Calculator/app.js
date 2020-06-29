a61bed78507fb77965bbc4662fd493dbb9af83fa
Zain Rehman <kh.zainrehman80@gmail.com>
2020-06-29 14:20:26 +0500

    Calculator final update

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,1 +1,19 @@
-alert("Hello Calculator")
+function getNum(num){
+    var result = document.getElementById("result")
+    result.value += num
+}
+
+function getResult(){
+    var result = document.getElementById("result")
+    result.value = eval(result.value)
+}
+
+function del(){
+    var result = document.getElementById("result")
+    result.value = result.value.substr(0,result.value.length-1)
+}
+
+function clearResult(){
+    var result = document.getElementById("result")
+    result.value = ""
+}
